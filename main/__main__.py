39e6efd1ec9a4b4308e78ddb6dc799cd55b52000
NikhilBakugo <90146942+NikhilBakugo@users.noreply.github.com>
2022-04-29 19:11:06 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -39,3 +39,9 @@ def restart(update, context):
         f.truncate(0)
         f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
     osexecl(executable, executable, "-m", "bot")
+
+def ping(update, context):
+    start_time = int(round(time() * 1000))
+    reply = sendMessage("Starting Ping", context.bot, update.message)
+    end_time = int(round(time() * 1000))
+    editMessage(f'{end_time - start_time} ms', reply)
